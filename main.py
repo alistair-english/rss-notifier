3918077bda92905158c428a1bdd330cf9249977e
Alistair English <alistair.english@protonmail.com>
2024-03-25 18:31:19 +1000

    date tracking works

--- main.py
+++ main.py
@@ -24,18 +24,25 @@ for url in FEED_URLS:
 
     feed = feedparser.parse(url)
 
+    if last_opened is None:
+        if len(feed["entries"]) < 1:
+            print("havent got any history and no entries to update history with!")
+            continue
 
+        print("havent got any history!")
+        last_published = dt.datetime.fromtimestamp(time.mktime(feed["entries"][0]["published_parsed"]))
+        with open(last_opened_path, 'w') as f:
+            f.write(last_published.isoformat())
+            continue
     
+    last_published = None
     for entry in reversed(feed["entries"]):
         entry_published = dt.datetime.fromtimestamp(time.mktime(entry["published_parsed"]))
-        if last_opened is None:
-            print("havent got any history!")
-            with open(last_opened_path, 'w') as f:
-                f.write(entry_published.isoformat())
-                break
-        
         if entry_published > last_opened:
             print(entry_published)
-            with open(last_opened_path, 'w') as f:
-                f.write(entry_published.isoformat())
-                break
+            last_published = entry_published
+    
+    if last_published is not None:
+        with open(last_opened_path, 'w') as f:
+            f.write(last_published.isoformat())
+            break
